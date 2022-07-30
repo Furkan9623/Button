0b0f73380a4477e097a6a0827a880e00a822ae4e
Furkan9623 <irctcticket81@gmail.com>
2022-07-30 18:12:53 +0530

    logn in page

---
+++ signup.js
@@ -0,0 +1,36 @@
+//get element statement;
+var arr = JSON.parse(localStorage.getItem("signup")) || [];
+
+//var arr = [];
+function signup(event) {
+  event.preventDefault();
+  var data = {
+    email: document.getElementById("sign_up").value,
+    number: document.getElementById("sign_num").value,
+    password: document.getElementById("pas").value,
+  };
+  arr.push(data);
+  //console.log(arr);
+
+  localStorage.setItem("signup", JSON.stringify(arr));
+  window.location.href = "./login.html";
+}
+
+function login(event) {
+  event.preventDefault();
+  var email = document.getElementById("login_email").value;
+  var password = document.getElementById("pas").value;
+
+  var flag = false;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].email === email && arr[i].password === password) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag) {
+    alert("Login Successfull");
+  } else {
+    alert("Enter correct email and password");
+  }
+}
